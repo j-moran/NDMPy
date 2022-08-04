ccea1315c4c03852e88fb7fd3aabab22b2d84bb5
John Moran <john.moran225@gmail.com>
2022-08-04 14:50:22 -0400

    Add example config file

---
+++ example-config.py
@@ -0,0 +1,15 @@
+#host config - ALL DATA STORED IN PLAINTEXT!!!! PLEASE CREATE USER WITH LIMITED ACCESS IN ORDER TO USE THIS APP!!!
+server_host 	= 'YOUR HOST HERE'
+username 		= 'YOUR USER HERE'
+password		= 'YOUR PASSWORD HERE'
+
+#Nagios Object Dictionaries - Add additional device types to the following dictionary in the format below
+device_types = {
+#	'Example Server' : {
+#		'name' 			:	'Example Server',
+#		'path'		 	: 	'PATH TO OBJECT FOLDER',
+#		'basetype'	 	: 	'windows_server',
+#		'OStype'  	 	: 	'windows',
+#		'platform'	 	:	'server'
+#	}
+}
