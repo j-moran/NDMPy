09cbc5322abf40f4759c8e71f70218df41bdad74
John Moran <john.moran225@gmail.com>
2022-08-17 16:56:57 -0400

    Add example services file

---
+++ example-services.py
@@ -0,0 +1,15 @@
+service_types = {
+	# Your Services Here
+	'general_services' : {
+		'Ping Check' : 'check_ping'
+	},
+	'windows_services' : {
+
+	},
+	'windows_server_services' : {
+
+	},
+	'linux_services' : {
+		
+	}
+}
